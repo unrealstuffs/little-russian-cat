a2ba78220b9f7ec36f6185a541b842ff0996d808
Olexander <slozhna0@gmail.com>
2022-03-22 21:39:34 +0500

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,104 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import { Swiper, SwiperSlide } from 'swiper/react'
+import { Parallax, Navigation, Mousewheel, FreeMode, Controller } from 'swiper'
+import { CSSTransition } from 'react-transition-group'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import 'swiper/css'
+import 'swiper/css/pagination'
+import 'swiper/css/navigation'
+import 'swiper/css/free-mode'
+import 'swiper/css/thumbs'
+
+import slides from './constants/slides'
+
+const App = () => {
+	const [controlledSwiper, setControlledSwiper] = useState(null)
+	const [showedSlide, setShowedSlide] = useState(0)
+	const [showDesc, setShowDesc] = useState(true)
+
+	return (
+		<>
+			<CSSTransition
+				in={showDesc}
+				timeout={350}
+				classNames='display'
+				unmountOnExit
+			>
+				<div className={`description`}>
+					<div className='logo'>Чмоня</div>
+					<p>
+						Маленький черный котенок, ставший мемом в рунете. На
+						наиболее известном снимке котенок сидит на панели
+						автомобиля. В конце 2021 года у котенка появилась кличка
+						– Чмоня. Именно так подписал картинку фильтр,
+						определяющий предметы по фото.
+					</p>
+				</div>
+			</CSSTransition>
+
+			<Swiper
+				freeMode={true}
+				centeredSlides={true}
+				onSlideChange={(e: any) => {
+					setShowDesc(e.activeIndex > 0 ? false : true)
+				}}
+				spaceBetween={60}
+				slidesPerView={3.5}
+				mousewheel={true}
+				parallax={true}
+				className={'slider slider__main'}
+				controller={{ control: controlledSwiper }}
+				modules={[
+					Parallax,
+					Navigation,
+					Mousewheel,
+					FreeMode,
+					Controller,
+				]}
+			>
+				{slides.map(({ id, dataParallax, img }) => (
+					<SwiperSlide
+						onClick={() =>
+							setShowedSlide(showedSlide !== 0 ? 0 : id)
+						}
+						className={`slider__item ${
+							showedSlide === id ? 'opened' : ''
+						}`}
+						key={id}
+					>
+						<div
+							className='slider__img'
+							data-swiper-parallax={dataParallax}
+							style={{
+								backgroundImage: `url(/${img})`,
+							}}
+						></div>
+					</SwiperSlide>
+				))}
+			</Swiper>
+			<Swiper
+				onSwiper={setControlledSwiper}
+				centeredSlides={true}
+				spaceBetween={60}
+				parallax={true}
+				slidesPerView={3.5}
+				className={'slider slider__bg'}
+				modules={[Parallax, Controller]}
+			>
+				{slides.map(({ id, dataParallax, img }) => (
+					<SwiperSlide className={'slider__item'} key={id}>
+						<div
+							className='slider__img'
+							data-swiper-parallax={dataParallax}
+							style={{
+								backgroundImage: `url(/${img})`,
+							}}
+						></div>
+					</SwiperSlide>
+				))}
+			</Swiper>
+		</>
+	)
 }
 
-export default App;
+export default App
---
+++ src/constants/slides.ts
@@ -0,0 +1,54 @@
+const slides = [
+	{
+		id: 1,
+		dataParallax: '20%',
+		img: 'assets/images/1.jpg',
+	},
+	{
+		id: 2,
+		dataParallax: '10%',
+		img: 'assets/images/2.jpg',
+	},
+	{
+		id: 3,
+		dataParallax: '15%',
+		img: 'assets/images/3.jpg',
+	},
+	{
+		id: 4,
+		dataParallax: '10%',
+		img: 'assets/images/4.jpg',
+	},
+	{
+		id: 5,
+		dataParallax: '15%',
+		img: 'assets/images/5.jpg',
+	},
+	{
+		id: 6,
+		dataParallax: '15%',
+		img: 'assets/images/6.jpg',
+	},
+	{
+		id: 7,
+		dataParallax: '10%',
+		img: 'assets/images/7.jpg',
+	},
+	{
+		id: 8,
+		dataParallax: '15%',
+		img: 'assets/images/8.jpg',
+	},
+	{
+		id: 9,
+		dataParallax: '10%',
+		img: 'assets/images/9.jpg',
+	},
+	{
+		id: 10,
+		dataParallax: '10%',
+		img: 'assets/images/10.jpg',
+	},
+]
+
+export default slides
